87fa22e265308055d01b799b35564fbe0367eb8a
ToatPor <88965520+ToatPor@users.noreply.github.com>
2022-06-10 11:43:44 +0700

    go

---
+++ script.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const section = document.querySelectorAll("section");
+
+// section.forEach((el) => (el.style.marginBottom = "9rem"));
+
+// const test = function () {
+//   navigator.geolocation.getCurrentPosition(
+//     function (position) {
+//       console.log(position);
+//     },
+//     function (err) {
+//       console.log(err);
+//     }
+//   );
+//   console.log(1);
+// };
+
+// test();
